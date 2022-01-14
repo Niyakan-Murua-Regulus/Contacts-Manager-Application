faabaf82e71f7667dbf5394ab5d9bb9c08b1c465
audrey murua <audrey.murua1@gmail.com>
2022-01-14 15:26:44 -0600

    added second file and code

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
+
 
 public class Contacts {
+    private String name;
+    private int number;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter an option (1, 2, 3, 4, or 5)");
-        System.out.println("1. View Contacts");
-        System.out.println("2. Add a new contact");
-        System.out.println("3. Search a contact by name");
-        System.out.println("4. Delete an existing contact");
-        System.out.println("5. Exit");
-
-
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
