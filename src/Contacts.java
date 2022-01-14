7485e23e4a8c8b9745692b56346ba3d197e8d657
audrey murua <audrey.murua1@gmail.com>
2022-01-14 11:20:59 -0600

    created basic file

---
+++ src/Contacts.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Contacts {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an option (1, 2, 3, 4, or 5)");
+        System.out.println("1. View Contacts");
+        System.out.println("2. Add a new contact");
+        System.out.println("3. Search a contact by name");
+        System.out.println("4. Delete an existing contact");
+        System.out.println("5. Exit");
+
+
+    }
+
+
+}
